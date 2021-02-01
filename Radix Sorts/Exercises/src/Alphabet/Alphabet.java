ad15871fe2febe7058311806d6b72029d2222868
Harrison832-beep <wharrison832@gmail.com>
2021-02-01 23:07:49 +0800

    Create folder Radix Sorts & . | Exercises

---
+++ Radix Sorts/Exercises/src/Alphabet/Alphabet.java
@@ -0,0 +1,39 @@
+package Alphabet;
+
+public class Alphabet {
+
+    public Alphabet(String s) {
+
+    }
+
+    public char toChar(int index) {
+
+        return '0';
+    }
+
+
+    public int toIndex(char c) {
+
+        return -1;
+    }
+
+    public boolean contains(char c) {
+        return false;
+    }
+
+    public int R() {
+        return -1;
+    }
+
+    public int lgR() {
+        return -1;
+    }
+
+    public int[] toIndices(String s) {
+        return null;
+    }
+
+    public String toChars(int[] indices) {
+        return null;
+    }
+}
