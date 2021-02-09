343d31fb4bef06dacbc2f2be39f06178d0af50fe
Harrison832-beep <wharrison832@gmail.com>
2021-02-10 00:02:41 +0800

    Add bookIndexer.java, work in progress

---
+++ Tries/Exercises/src/BookIndexer.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BookIndexer {
+    public static void main(String[] args) {
+        String s = StdIn.readAll();
+        StdOut.println(s);
+
+    }
+}
